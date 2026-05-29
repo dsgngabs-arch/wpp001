4dd11cdbaf896fc5a9a0e2685a4cd13e09b4a3a4
dsgngabs-arch <dsgngabs@gmail.com>
2026-05-29 12:01:19 -0300

    Update CTAFinalSection.tsx

--- client/src/components/CTAFinalSection.tsx
+++ client/src/components/CTAFinalSection.tsx
@@ -14,7 +14,7 @@ export default function CTAFinalSection() {
     <section 
       className="relative w-full py-20 lg:py-32 bg-white overflow-hidden"
       style={{
-        backgroundImage: 'url(/manus-storage/mrbet-white-bg_6b5819e0.png)',
+        backgroundImage: `url("${BENEFITS_BG}")`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundAttachment: 'fixed'
